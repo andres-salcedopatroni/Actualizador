02ccc905658909147ed6b92549122997b9280c1c
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-03-04 23:20:57 -0500

    Nuevo

--- app.js
+++ app.js
@@ -4,11 +4,67 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
-
+var cron = require('node-cron');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//Conexion Atlas
+const mongoose = require('mongoose');
+const axios = require('axios');
+//Mongo
+mongoose.connect('mongodb+srv://USER:1234@cluster0.4wtcxn6.mongodb.net/Tesis?retryWrites=true&w=majority');
+const schema_estudiantes=require('./schemas/schema_estudiantes');
+const schema_tweets=require('./schemas/schema_tweets');
+const estudiantes = mongoose.model('Estudiantes', schema_estudiantes,'Estudiantes');
+const tweets = mongoose.model('Tweets', schema_tweets,'Tweets');
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+cron.schedule('0 0 * * *', async function(){
+  const date = new Date();
+  const lista_estudiante=await estudiantes.find({});
+  var grupos=[];
+  var usuarios=[];
+  var numero=0;
+  for (const e of lista_estudiante){
+    usuarios.push({"id":e.usuario,"fecha":date});
+    numero=numero+1;
+    if(numero==15 || lista_estudiante.indexOf(e)+1==lista_estudiante.length){
+      grupos.push(usuarios);
+      usuarios=[];
+      numero=0;
+    }
+  }
+  for (const g of grupos){
+    axios.post("https://andressalcedo2023.pythonanywhere.com//actualizar_tweets",g)
+    .then(datos => {
+      const tweets_usuarios=datos.data;
+      try{
+        for (const tweets_usuario of tweets_usuarios){
+          for (const tweet of tweets_usuario.usuario){
+            const t=new tweets({
+              estado: tweet.estado,
+              mensaje: tweet.texto, 
+              fecha: tweet.fecha,
+              usuario: tweets_usuario.usuario
+            });
+            t.save();
+          }
+        }
+      }catch(error){
+        console.log(error);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
+    await sleep(60000*20);
+  }
+  }
+);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ routes/estudiantes.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var router = express.Router();
+//Modulos
+const axios = require('axios');
+const mongoose = require('mongoose');
+//Mongo
+const schema_estudiantes=require('../schemas/schema_estudiantes');
+const schema_tweets=require('../schemas/schema_tweets');
+const estudiantes = mongoose.model('Estudiantes', schema_estudiantes,'Estudiantes');
+const tweets = mongoose.model('Tweets', schema_tweets,'Tweets');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/mostrar', async function(req, res, next) {
+  try{
+  const e=await estudiantes.find({});
+  res.json(e);}
+  catch{}
+});
+
+router.get('/obtener/:usuario', async function(req, res, next) {
+  try{
+  const usuario=req.params.usuario
+  const e=await estudiantes.findOne({usuario:usuario});
+  const t=await tweets.find({usuario:usuario}).sort({fecha: -1});;
+  res.json({"estudiante":e,"tweets":t});}
+  catch{}
+});
+
+router.post('/agregar', function(req, res, next) {
+  
+  const pedido=req.body;
+  let respuesta;
+  axios.post("https://andressalcedo2023.pythonanywhere.com/tweets",{"usuario": pedido.usuario})
+  .then(datos => {
+    const tweets_usuario=datos.data;
+    const e=new estudiantes({
+      nombre: pedido.nombre, 
+      usuario: pedido.usuario,
+      codigo: pedido.codigo,
+      correo: pedido.correo,
+      celular: pedido.celular,});
+    e.save();
+    try{
+      for (const m of tweets_usuario){
+        const t=new tweets({
+          estado: m.estado,
+          mensaje: m.texto, 
+          fecha: m.fecha,
+          usuario: pedido.usuario
+        });
+        t.save();
+      }
+      res.json({'mensaje':1});
+    }catch(error){
+      res.status(500).send(err);
+    }
+  })
+  .catch(err => {
+    res.status(500).send(err);
+  });
+  
+});
+
+router.delete('/eliminar', async function(req, res, next) {
+  try{
+    const pedido=req.body;
+    var eliminados=0;
+    for (const e of pedido.eliminar){
+      var num = await estudiantes.deleteMany({usuario: e});
+      eliminados=num.deletedCount+eliminados;
+    }
+    res.json({"eliminados":eliminados});
+  }
+  catch(error){
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
