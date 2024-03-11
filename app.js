306aef48243d9883c8b9a38a2ada49a499e50a3d
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-03-11 04:06:56 -0500

    Nuevo

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+console.log("Hola mundo")
 var app = express();
 
 //Manejo de tweets
